39f2161a0ebda860cd7c4fc1b9f94cd2dc340a73
Talisson Melo <talis.melo@petize.com.br>
2024-07-19 23:28:42 -0300

    Rolagem em Tela

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -6,77 +6,83 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.grey,
-      body: Column(
-        children: [
-          Expanded(
-              child: Container(
-            color: Colors.grey,
-          )),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const CustomTextField(
-                  icon: Icons.email,
-                  label: 'Email',
-                ),
-                const CustomTextField(
-                  icon: Icons.lock,
-                  label: 'Senha',
-                  isSecret: true,
-                ),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18))),
-                      onPressed: () {},
-                      child: const Text('Entrar',
-                          style: TextStyle(fontSize: 18, color: Colors.white))),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Esqueceu a senha?',
-                        style: TextStyle(color: Colors.blue),
+      body: SingleChildScrollView(
+          child: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          children: [
+            Expanded(child: Container()),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(45))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const CustomTextField(
+                    icon: Icons.email,
+                    label: 'Email',
+                  ),
+                  const CustomTextField(
+                    icon: Icons.lock,
+                    label: 'Senha',
+                    isSecret: true,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18))),
+                        onPressed: () {},
+                        child: const Text('Entrar',
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.white))),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(color: Colors.blue),
+                        )),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Divider(
+                        color: Colors.grey.withAlpha(90),
+                        thickness: 2,
                       )),
-                ),
-                Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Divider(
-                      color: Colors.grey.withAlpha(90),
-                      thickness: 2,
-                    )),
-                SizedBox(
-                  height: 50,
-                  child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(
-                          width: 2,
-                          color: Colors.grey,
+                  SizedBox(
+                    height: 50,
+                    child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            width: 2,
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        'Criar conta',
-                        style: TextStyle(color: Colors.grey, fontSize: 18),
-                      )),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Criar conta',
+                          style: TextStyle(color: Colors.grey, fontSize: 18),
+                        )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
