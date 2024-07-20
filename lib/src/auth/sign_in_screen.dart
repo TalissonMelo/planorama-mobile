662712b8485f2fb4a4b03e24a617bdcb1e9542df
Talisson Melo <talis.melo@petize.com.br>
2024-07-19 21:43:30 -0300

    Adicionando o campo de texto

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -15,9 +15,27 @@ class SignInScreen extends StatelessWidget {
           )),
           Expanded(
               child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
             decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    isDense: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18)),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18)),
+                  ),
+                )
+              ],
+            ),
           ))
         ],
       ),
